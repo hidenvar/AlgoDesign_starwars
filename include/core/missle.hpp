99f655895f835e2e4f292692812ff5cbe5a812af
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-04 19:20:28 +0330

    feat(missle): add constructor to the base class

--- include/core/missle.hpp
+++ include/core/missle.hpp
@@ -1,9 +1,12 @@
 #pragma once
+#include <streambuf>
 #include <string>
 #include "missle_type.hpp"
 
 class Missle {
   public:
+    Missle(std::string name, int od, int ud, int des, int st, MissleType type) :
+      name(name), overalDistance(od), uncontrolledDistance(ud), destruction(des), stealth(st), type(type) {}
   private:
       std::string name;
       int overalDistance;
