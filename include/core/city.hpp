d135f3d62e453093bbef8e85020e17ea6db94c89
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-04 13:50:15 +0330

    feat(core): add 'City' base class

---
+++ include/core/city.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class City {
+  public:
+  private:
+      std::string cityName;
+      std::string countryName;
+      int latitude;
+      int longitude;
+      CityType type;
+};
