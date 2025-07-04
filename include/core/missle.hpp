d1b97cad185f6699f600d90b80265b13aa09c672
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-04 13:20:28 +0330

    feat(core): add 'Missle' base class

---
+++ include/core/missle.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+class Missle {
+  public:
+  private:
+      std::string name;
+      int overalDistance;
+      int uncontrolledDistance;
+      int destruction;
+      int stealth;
+      MissleType type;
+};
