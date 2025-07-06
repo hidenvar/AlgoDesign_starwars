040d2aa94f9257af73f916aaca7b52e96ed2de1e
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-06 20:29:45 +0330

    Revert "refactor(spy): fix typo"
    This reverts commit 7edb482bd36d419d5b531eb527c9359ed37c31bd.

--- src/core/city.cpp
+++ src/core/city.cpp
@@ -1,17 +1,17 @@
 #include "../../include/core/city.hpp"
 
 City::City(const std::string& name, const std::string& country, double lat,
-           double lon, CityType type, bool sp)
+           double lon, CityType type, int sp)
     : cityName(name),
       countryName(country),
       latitude(lat),
       longitude(lon),
       type(type),
-      spy(sp) {}
+      spies(sp) {}
 
 std::string City::getName() const { return cityName; }
 std::string City::getCountry() const { return countryName; }
 double City::getLatitude() const { return latitude; }
 double City::getLongitude() const { return longitude; }
 CityType City::getType() const { return type; }
-bool City::hasSpy() const { return spy; }
+bool City::hasSpy() const { return hasSpy; }
