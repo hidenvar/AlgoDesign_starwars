658e6df609c2cc564808fbe34e1a403e15f4cbc1
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-06 18:04:15 +0330

    feat(base_city): add BaseCity class

---
+++ src/core/base_city.cpp
@@ -0,0 +1,7 @@
+#include "../../include/core/base_city.hpp"
+
+BaseCity::BaseCity(const std::string& name, const std::string& country, double lat, 
+                   double lon, CityType type, int sp, std::vector<std::pair<Missile, int>> mc)
+        : City(name, country, lat, lon, type, sp), missileCount(mc) {}
+    
+    
