9f5f858818d28e4746127aefc905145d709cf9d0
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-07 15:25:16 +0330

    feat(target_city): add getter for 'defenseLevel'

--- src/core/target_city.cpp
+++ src/core/target_city.cpp
@@ -3,3 +3,5 @@
 TargetCity::TargetCity(const std::string& name, const std::string& country, double lat,
                        double lon, CityType type, bool sp, int dl)
           : City(name, country, lat, lon, type, sp), defenseLevel(dl) {}
+
+int TargetCity::getDefenseLevel() const { return defenseLevel; }
