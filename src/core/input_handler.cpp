9602e164e9d0c0b2db782d990519fe05ca6eed5e
hidenvar <s.amirm.fazeli@gmail.com>
2025-07-07 15:27:06 +0330

    feat(input-handler): add support for parsing all city types from input

--- src/core/input_handler.cpp
+++ src/core/input_handler.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include "../../include/core/input_handler.hpp"
 #include "../../include/core/city_type.hpp"
+#include "../../include/core/base_city.hpp"
+#include "../../include/core/target_city.hpp"
 
 void InputHandler::loadFromFile(Graph &citiesGraph) {
   if (!freopen("map.txt", "r", stdin)) {
@@ -20,12 +22,31 @@ void InputHandler::makeGraph(std::istream& input, Graph& citiesGraph) {
     std::istringstream iss(line);
     std::string name, country;
     CityType type;
-    double lat, lon;
+    int lat, lon;
     int sp;
 
     iss >> name >> country >> lat >> lon >> type >> sp;
-    
-  }
 
+    if (type == CityType::NORMAL) {
+      auto normalCity = std::make_shared<City>(name, country, lat, lon, type, sp);
+      citiesGraph.addCity(normalCity);
+    }
+    else if (type == CityType::BASE) {
+      std::vector<std::pair<Missile, int>> missiles;
+      std::string missileName;
+      int missileCount;
+      while(iss >> missileName >> missileCount) {
+        // missiles.emplace_back(missileName, missileCount); // todo : need completed missile implementation
+      }
+      auto baseCity = std::make_shared<BaseCity>(name, country, lat, lon, type, sp, missiles);
+      citiesGraph.addCity(baseCity);
+    }
+    else if (type == CityType::TARGET) {
+      int defenseLevel;
+      iss >> defenseLevel;
+      auto targetCity = std::make_shared<TargetCity>(name, country, lat, lon, type, sp, defenseLevel);
+      citiesGraph.addCity(targetCity);
+    }
+  }
 }
 
