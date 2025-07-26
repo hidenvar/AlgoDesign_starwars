dc3280badf733b5945121774548541916ecfe0dc
amir mohammad fazeli <s.amirm.fazeli@gmail.com>
2025-07-26 14:26:30 +0330

    feat(scenario2): add pathfinding logic and PathInfo struct

--- include/scenarios/scenario2.hpp
+++ include/scenarios/scenario2.hpp
@@ -8,4 +8,18 @@ class Scenario2 : public Scenario {
         Scenario2(Graph& g);
         void initialize() override;
         void solve() override;
+    
+    private:
+      struct PathInfo {
+        Graph::VertexDescriptor base;
+        Graph::VertexDescriptor target;
+        std::vector<std::string> cities;
+        int spyCount;
+        double distance;
+      };
+        void findPaths();
+
+        std::vector<Graph::VertexDescriptor> baseVertices;
+        std::vector<Graph::VertexDescriptor> targetVertices;
+        std::vector<PathInfo> paths; 
 };
